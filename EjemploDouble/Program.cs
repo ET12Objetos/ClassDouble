842b999c7d2f37648bcddecb05be4484b5547a2b
jvelazquez <jonathanvgms@gmail.com>
2019-11-24 22:57:14 -0300

    Add Program.cs

---
+++ EjemploDouble/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EjemploDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double promedio = (7d + 3d + 4d) / 3d;
+            System.Console.WriteLine("{0:0.00}", promedio);
+        }
+    }
+}
